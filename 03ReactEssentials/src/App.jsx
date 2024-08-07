7ae2ca27183fa2fffe69501db168f13d6df844ce
devYuraKim <dev.yurakim@gmail.com>
2024-08-07 21:22:04 +0900

    work with props

--- 03ReactEssentials/src/App.jsx
+++ 03ReactEssentials/src/App.jsx
@@ -1,3 +1,4 @@
+import { CORE_CONCEPTS } from "./data";
 import reactImg from "./assets/react-core-concepts.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
@@ -20,12 +21,33 @@ function Header() {
   );
 }
 
+function CoreConcepts() {
+  return CORE_CONCEPTS.map((concept) => (
+    <CoreConcept concept={concept} key={concept.title} />
+  ));
+}
+
+function CoreConcept({ concept }) {
+  return (
+    <li>
+      <img src={concept.image} alt={concept.title} />
+      <h3>{concept.title}</h3>
+      <p>{concept.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcepts />
+          </ul>
+        </section>
       </main>
     </div>
   );
