75294e0324e81b55fcd143b443029092120899e4
devYuraKim <dev.yurakim@gmail.com>
2024-08-07 23:22:50 +0900

    access object properties w/ bracket notation

--- 03ReactEssentials/src/App.jsx
+++ 03ReactEssentials/src/App.jsx
@@ -2,13 +2,13 @@ import Header from "./components/Header";
 import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
 import { useState } from "react";
+import { EXAMPLES } from "./data";
 
 function App() {
   const [tabContent, setTabContent] = useState("Please click a button");
 
   function handleClick(input) {
-    setTabContent(input);
-    console.log(input);
+    setTabContent(EXAMPLES[input].description);
 
     // == resolve setState lagging problem by directly capturing value
     // const newTabContent = input;
@@ -30,10 +30,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={handleClick}>components</TabButton>
+            <TabButton onClick={handleClick}>jsx</TabButton>
+            <TabButton onClick={handleClick}>props</TabButton>
+            <TabButton onClick={handleClick}>state</TabButton>
           </menu>
           {tabContent}
         </section>
