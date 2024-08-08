ea7399322b6dc688f1c7aca3863564f849db1188
devYuraKim <dev.yurakim@gmail.com>
2024-08-08 15:25:11 +0900

    pass tags as props

--- 04EssentialsDeepDive/src/components/Examples.jsx
+++ 04EssentialsDeepDive/src/components/Examples.jsx
@@ -18,6 +18,7 @@ export default function Examples() {
   return (
     <Section id="examples" title="Examples">
       <Tabs
+        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
