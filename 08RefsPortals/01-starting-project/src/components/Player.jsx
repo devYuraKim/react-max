7570f38b358a56c3e4051b2e86f58c949bceaef7
devYuraKim <dev.yurakim@gmail.com>
2024-08-15 22:43:23 +0900

    bypass file input styling problem

--- 08RefsPortals/01-starting-project/src/components/Player.jsx
+++ 08RefsPortals/01-starting-project/src/components/Player.jsx
@@ -4,9 +4,20 @@ export default function Player() {
   const [name, setName] = useState(null);
   const playerName = useRef();
 
+  const filePicker = useRef();
+  const [fileName, setFileName] = useState(null);
+
   function handleClick() {
     setName(playerName.current.value);
     playerName.current.value = "";
+  }
+
+  function handleClickFilePicker() {
+    filePicker.current.click();
+  }
+
+  function handleFilePickerChange(e) {
+    setFileName(e.target.files[0].name);
   }
 
   return (
@@ -16,6 +27,15 @@ export default function Player() {
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={filePicker}
+        onChange={handleFilePickerChange}
+      />
+      <input type="text" readOnly={true} value={fileName} />
+      <button onClick={handleClickFilePicker}>Pick Image</button>
     </section>
   );
 }
